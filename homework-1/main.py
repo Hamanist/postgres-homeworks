b1b7d55f05e7eb7aec2570d46cefe39ed15bc39f
Hamanist <hamanist@gmail.com>
2025-09-12 19:37:40 +0300

    created a connection to the database

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,28 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+from dotenv import load_dotenv
+import os
+import psycopg2
+
+
+load_dotenv()
+try:
+    with psycopg2.connect(
+            host=os.getenv('DB_HOST'),
+            database=os.getenv('DB_NAME'),
+            user=os.getenv('DB_USER'),
+            password=os.getenv('DB_PASSWORD'),
+            port=os.getenv('DB_PORT')
+    ) as connection:
+        with connection.cursor() as cursor:
+            cursor.execute(
+                'SELECT version();'
+            )
+            print(f'СЕРВЕР {cursor.fetchone()}')
+
+
+except Exception as ex:
+    print(f'Что то не так {ex}')
+finally:
+    if not connection:
+        connection.close()
